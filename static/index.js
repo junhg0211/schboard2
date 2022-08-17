c327a962b29b6ca8bf6792f9d82958e81eed6b95
Sch <junhg0211@gmail.com>
2022-08-17 22:43:08 +0900

    getclosestpointtosegment: debug when x1 > x2 or y1 > y2, horizontal or vertical

--- static/index.js
+++ static/index.js
@@ -10,9 +10,11 @@ function limit(t, a, b) {
 
 function getClosestPointToSegment(x0, y0, x1, y1, x2, y2) {
   if (x1 === x2) {
+    if (y2 < y1) [y1, y2] = [y2, y1];
     return [x1, limit(y0, y1, y2)];
   }
   if (y1 === y2) {
+    if (x2 < x1) [x1, x2] = [x2, x1];
     return [limit(x0, x1, x2), y1];
   }
 
